11b65a2ce797bf25ce0d9293a03400d275290ebd
Dominic-Taylor-Dev <email.dom.taylor@gmail.com>
2021-03-16 16:03:53 +0000

    To chapter 10

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -3,16 +3,38 @@
 # this class provides blueprint for oystercard instances
 class Oystercard
   MAX_BALANCE = 90
+  MIN_BALANCE = 0
 
   attr_reader :balance
-
+  
   def initialize
     @balance = 0
+    @in_journey = false
   end
 
   def top_up(amount)
     raise "Max balance is #{MAX_BALANCE}" if amount + @balance > MAX_BALANCE
 
     @balance += amount
+  end
+
+  def deduct(amount)
+    raise "Min balance is #{MIN_BALANCE}" if @balance - amount < MIN_BALANCE
+
+    @balance -= amount
+  end
+
+  def in_journey?
+    @in_journey
+  end
+
+  def touch_in
+    raise 'You need at least £1 to touch in' if @balance < 1
+    
+    @in_journey = true
+  end
+
+  def touch_out
+    @in_journey = false
   end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -13,7 +13,44 @@ describe Oystercard do
     end
 
     it 'throws exception if trying to top up to more than max balance' do
-      expect { subject.top_up(Oystercard::MAX_BALANCE + 1) }.to raise_error("Max balance is #{MAX_BALANCE}")
+      expect { subject.top_up(Oystercard::MAX_BALANCE + 1) }.to raise_error("Max balance is #{Oystercard::MAX_BALANCE}")
+    end
+  end
+
+  context 'when balance starts non-zero (10)' do
+    before(:each) do
+      subject.top_up(10)
+    end
+    
+    describe '#touch_in' do
+      it 'changes in_journey status from false to true' do
+        subject.touch_in
+        expect(subject.in_journey?).to eq(true)
+      end
+    end
+  
+    describe '#touch_out' do
+      it 'changes in_journey status from true to false' do
+        subject.touch_in
+        subject.touch_out
+        expect(subject.in_journey?).to eq(false)
+      end
+    end
+
+    describe '#deduct' do
+      it 'can decrease balance' do
+        expect { subject.deduct 1 }.to change { subject.balance}. by(-1)
+      end
+
+      it 'throws an exception if trying to deduct more than min balance allows' do
+        expect { subject.deduct(subject.balance - Oystercard::MIN_BALANCE + 1)}.to raise_error("Min balance is #{Oystercard::MIN_BALANCE}")
+      end
+    end
+  end
+
+  describe '#in_journey?' do
+    it 'in_journey should be false by default' do
+      expect(subject).not_to be_in_journey
     end
   end
 end
